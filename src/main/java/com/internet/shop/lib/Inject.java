36bdc97d5c2661e3a9a2c1c37151cb67667eff00
Vladislav <48126402+vladislavshvets@users.noreply.github.com>
2020-09-04 15:35:06 +0300

    back to start

--- src/main/java/com/internet/shop/lib/Inject.java
+++
@@ -1,8 +0,0 @@
-package com.internet.shop.lib;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Inject {
-}
